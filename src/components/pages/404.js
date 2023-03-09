d3a7369796818c90d6d5a3c2ad2aff6cbc2d600b
mstoletov <stoletov.police@gmail.com>
2023-03-09 15:17:57 +1000

    changed 404

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import doge from "../../resources/img/doge.png"
+import rocket from "../../resources/img/sad-rocket.gif"
 
 const Page404 = () => {
     return (
         <div>
             <div>
-                <img style={{'height': '500px', 'weight': '500px', 'display': 'block', 'margin': '0 auto'}} src={doge} alt="doge" />
-                <h1 style={{'fontSize': '50px', 'textAlign': 'center','margin': '30px'}}>Wow, such empty</h1>
+                <img style={{'height': '500px', 'weight': '500px', 'display': 'block', 'margin': '0 auto'}} src={rocket} alt="doge" />
+                <h1 style={{'fontSize': '50px', 'textAlign': 'center','margin': '30px'}}>{`Oops, nothing there:(`}</h1>
                 <Link to='/' style={{'font-family':'Roboto Condensed', 'fontSize': '30px', 'textAlign': 'center', 'display': 'block', 'margin': '0 auto', 'text-decoration': 'underline'}}>Back to main page</Link>
             </div>
         </div>
